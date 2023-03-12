20657242ada015b4ed6051e297afdaeba3f50314
heidfeld <l.karmanski@gmail.com>
2023-03-12 18:48:01 +0100

    + added youtube header

--- src/examples/Header/Header.js
+++ src/examples/Header/Header.js
@@ -1,8 +1,29 @@
+import React, {useCallback} from "react";
+
+import './Header.css';
+
 const Header = () => {
+
+    const renderYouTubeHeader = useCallback(() => {
+        return (
+            <div className="yt-header">
+                <div className="yt-header__left">
+                    <div id="yt-logo-container">Logo</div>
+                </div>
+                <div className="yt-header__center">
+                    <div id="yt-search-container">Search</div>
+                </div>
+                <div className="yt-header__right">
+                    <div id="yt-user-container">User</div>
+                </div>
+            </div>
+        );
+    }, []);
 
     return (
         <div>
-
+            YouTube Header
+            {renderYouTubeHeader()}
         </div>
     );
 
